348963cb4017aca537219ae5192be2a16d8e0a53
Michał Sternik <260455@student.pwr.edu.pl>
2024-03-06 10:58:30 +0100

    reformated cpp code

--- Lab_1/Lab1_cpp/main.cpp
+++ Lab_1/Lab1_cpp/main.cpp
@@ -3,21 +3,22 @@
 #include <vector>
 #include <algorithm>
 #include <chrono>
+using namespace std;
 
-int median_low(std::vector<int> array) {
-    std::vector<int> sorted_array = array;
-    std::sort(sorted_array.begin(), sorted_array.end());
+int median_low(vector<int> array) {
+// we work on array copy so theres no need to do anything
+    sort(array.begin(), array.end());
     int median_low;
-    if (sorted_array.size() % 2 == 0) {
-        median_low = sorted_array[sorted_array.size() / 2 - 1];
+    if (array.size() % 2 == 0) {
+        median_low = array[array.size() / 2 - 1];
     } else {
-        median_low = sorted_array[sorted_array.size() / 2];
+        median_low = array[array.size() / 2];
     }
     return median_low;
 }
 
-void write_result_to_file(std::string filename, std::vector<int> data) {
-    std::ofstream file(filename);
+void write_result_to_file(string filename, vector<int> data) {
+    ofstream file(filename);
     if (file.is_open()) {
         for (int i = 0; i < data.size(); i++) {
             file << data[i];
@@ -28,73 +29,78 @@ void write_result_to_file(std::string filename, std::vector<int> data) {
         file << "\n";
         file.close();
     } else {
-        std::cout << "Unable to open file";
+        cout << "Unable to open file";
     }
 }
 
-std::vector<std::vector<int>> read_data_from_file(std::string file_path) {
-    std::vector<std::vector<int>> data;
-    std::ifstream file(file_path);
+vector<vector<int>> read_data_from_file(string file_path) {
+    vector<vector<int>> data;
+    ifstream file(file_path);
 
-//    std::ifstream file(file_path);
+//    ifstream file(file_path);
     if (file.is_open()) {
-        std::string line;
-        while (std::getline(file, line)) {
-            std::vector<int> row;
+        string line;
+        while (getline(file, line)) {
+            vector<int> row;
             size_t pos = 0;
-            std::string token;
-            while ((pos = line.find(',')) != std::string::npos) {
+            string token;
+            while ((pos = line.find(',')) != string::npos) {
                 token = line.substr(0, pos);
-                row.push_back(std::stoi(token));
+                row.push_back(stoi(token));
                 line.erase(0, pos + 1);
             }
-            row.push_back(std::stoi(line));
+            row.push_back(stoi(line));
             data.push_back(row);
         }
         file.close();
     } else {
-        std::cout << "Unable to open file1";
+        cout << "Unable to open file";
     }
     return data;
 }
 
 void test_correctness() {
-    std::vector<std::vector<int>> loaded_data = read_data_from_file("C:\\Users\\march3wa\\Desktop\\Jezyki Skryptowe\\Lab_1\\test_data.txt");
-    std::vector<int> results;
-    for (std::vector<int> test_set : loaded_data) {
+    vector<vector<int>> loaded_data = read_data_from_file("../../test_data.txt");
+    vector<int> results;
+    for (vector<int> test_set : loaded_data) {
         int result = median_low(test_set);
         results.push_back(result);
     }
-    write_result_to_file("C:\\Users\\march3wa\\Desktop\\Jezyki Skryptowe\\Lab_1\\cpp_result_data.txt", results);
+    write_result_to_file("../../cpp_result_data.txt", results);
 }
 
 void test_performance(int iterations) {
-    std::vector<std::vector<int>> loaded_data = read_data_from_file("C:\\Users\\march3wa\\Desktop\\Jezyki Skryptowe\\Lab_1\\test_data.txt");
+    vector<vector<int>> loaded_data = read_data_from_file("../../test_data.txt");
+    typedef chrono::high_resolution_clock clock;
+    typedef chrono::milliseconds ms;
+    typedef chrono::duration<float> fsec;
 
-    auto start_time = std::chrono::high_resolution_clock::now();
+    auto start_time = clock::now();
     for (int i = 1; i < iterations; i++) {
-        for (std::vector<int> test_set : loaded_data) {
+        for (vector<int> test_set : loaded_data) {
             // Do nothing
         }
     }
-    auto end_time = std::chrono::high_resolution_clock::now();
-    double empty_time = std::chrono::duration_cast<std::chrono::duration<double>>(end_time - start_time).count();
-    std::cout << "Empty Time: " << empty_time << " seconds" << std::endl;
+    auto end_time = clock::now();
+    fsec empty_time = end_time - start_time;
+//    cout << "Empty Time: " << empty_time << " seconds" << endl;
 
-    start_time = std::chrono::high_resolution_clock::now();
+    start_time = clock::now();
     for (int i = 1; i < iterations; i++) {
-        for (std::vector<int> test_set : loaded_data) {
+        for (vector<int> test_set : loaded_data) {
             median_low(test_set);
         }
     }
-    end_time = std::chrono::high_resolution_clock::now();
-    double execution_time = std::chrono::duration_cast<std::chrono::duration<double>>(end_time - start_time).count() - empty_time;
-    std::cout << "Execution Time: " << execution_time << " seconds" << std::endl;
+    end_time = clock::now();
+    fsec execution_time = end_time - start_time - empty_time;
+    cout << "Execution Time: " << execution_time << " seconds" << endl;
 }
 
 int main() {
-    test_correctness();
-//    test_performance(10000);
+//    test_correctness();
+    for (int i = 1; i < 100; i++) {
+        test_performance(10000);
+    }
     return 0;
 }
 
